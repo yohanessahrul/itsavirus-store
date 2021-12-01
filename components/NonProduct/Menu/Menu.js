83e44470f0ba37303168fdfca0ec97ebdf351e59
yohanessahrul <yohanessahrul92@gmail.com>
2021-12-01 22:18:34 +0700

    deploy 3

--- components/NonProduct/Menu/Menu.js
+++ components/NonProduct/Menu/Menu.js
@@ -20,9 +20,9 @@ function Menu(props) {
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container">
           <Link href="/">
-            <a className="navbar-brand">
+            <div className="navbar-brand">
               <Icon name="ico-nike" viewBox="0 0 58 21" width={56} fill="#000000" stroke="none" />
-            </a>
+            </div>
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -31,51 +31,55 @@ function Menu(props) {
             <ul className="navbar-nav mx-auto">
               <li className="nav-item pe-3">
                 <Link href="/#">
-                  <a className="nav-link active">New Release</a>
+                  <div className="nav-link active">
+                    New Release
+                  </div>
                 </Link>
               </li>
               <li className="nav-item pe-3">
                 <Link href="/#">
-                  <a className="nav-link">Men</a>
+                  <div className="nav-link">
+                    Men
+                  </div>
                 </Link>
               </li>
               <li className="nav-item pe-3">
-                <a className="nav-link" href="/#">Women</a>
+                <div className="nav-link" href="/#">Women</div>
               </li>
               <li className="nav-item  pe-3">
-                <a className="nav-link" href="/#">Kids</a>
+                <div className="nav-link" href="/#">Kids</div>
               </li>
               <li className="nav-item  pe-3">
-                <a className="nav-link" href="/#">Customize</a>
+                <div className="nav-link" href="/#">Customize</div>
               </li>
             </ul>
             <div className="float-lg-end">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 mb-4 ms-lg-5 py-2">
                 <li className="nav-item">
-                  <a className="nav-link" href="/bag">
+                  <div className="nav-link" href="/bag">
                     <div className={classes.Icon}>
                       {props.cartRedux ? <div className={classes.Badge}>{props.cartRedux ? props.cartRedux.length : ''}</div> : null}
                       <Icon name="ico-bag" width={24} fill="#000000" stroke="none" />
                     </div>
-                  </a>
+                  </div>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/#">
+                  <div className="nav-link" href="/#">
                     <div className={classes.Icon}>
                       <Icon name="ico-user-circle" width={30} fill="#000000" stroke="none" />
                     </div>
-                  </a>
+                  </div>
                 </li>
               </ul>
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 mb-4 ms-lg-5 py-2 d-block d-sm-block d-md-none">
                 <li className="nav-item pe-3">
-                  <a className="nav-link" href="/#">Shopping</a>
+                  <div className="nav-link" href="/#">Shopping</div>
                 </li>
                 <li className="nav-item  pe-3">
-                  <a className="nav-link" href="/#">FAQ</a>
+                  <div className="nav-link" href="/#">FAQ</div>
                 </li>
                 <li className="nav-item  pe-3">
-                  <a className="nav-link" href="/#">Contact</a>
+                  <div className="nav-link" href="/#">Contact</div>
                 </li>
               </ul>
 
